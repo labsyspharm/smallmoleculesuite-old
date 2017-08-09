a37d43278db04b468b3bda279d6a2bad7113d7b2
Nicholas Clark <clarkno@mail.uc.edu>
2017-08-09 17:05:34 -0400

    Most of UI, with filler table

--- custom_library_app/server.R
+++ custom_library_app/server.R
@@ -5,19 +5,24 @@ library(readr)
 
 library_table = read_csv("selection_table_cmpds_best_and_second_class.csv")
 all_genes = unique(library_table$gene_id)
-print(all_genes)
+print(head(library_table))
 
 shinyServer(function(input, output, session) {
   # Define reactive values
-  values = reactiveValues(gene_list = NULL, genes_not_found = NULL, gene_list_found = 0)
+  values = reactiveValues(gene_list = NULL, genes_not_found = NULL, gene_list_found = 0,
+                          genes_not_found_paste = NULL)
 
   # Make sure genes given are in the genes that we have info for
   observeEvent(eventExpr = input$gene_list, handlerExpr = {
     values$gene_list = unlist(strsplit(input$gene_list, "\n"))
-    values$genes_not_found = paste(values$gene_list[!values$gene_list %in% all_genes], collapse = ", ")
-    output$search_genes = renderText({ paste("Genes not found:", values$genes_not_found) })
+    values$genes_not_found = values$gene_list[!values$gene_list %in% all_genes]
+    values$genes_not_found_paste = paste(values$genes_not_found, collapse = ", ")
+    output$search_genes = renderText({ paste(length(values$genes_not_found), "gene(s) not found:", values$genes_not_found_paste) })
     values$gene_list_found = values$gene_list[values$gene_list %in% all_genes]
     output$gene_total = renderText({ paste(length(values$gene_list_found), "gene(s) entered.") })
-  }, ignoreInit = T)
+  })
+  
+  # Output table: (change later)
+  output$output_table = renderDataTable(library_table)
 
 })
--- custom_library_app/ui.R
+++ custom_library_app/ui.R
@@ -7,11 +7,50 @@ shinyUI(
     title = "My page",
     shinyjs::useShinyjs(),
     suppressDependencies("bootstrap"),
-    div(class = "ui form", label = "Gene list",
-        textAreaInput(inputId = "gene_list", label = "Gene list", value = "", placeholder = "MTOR\nRPS6K1\nAKT")
+    br(),
+    div(class="ui container segment",
+      div(class = "ui three column centered grid",
+        div(class = "column",
+          h2("Custom library app")
+        )
+      ),
+      div(class = "ui stackable three column centered grid", 
+        div(class = "column",
+          a(class = "ui red ribbon label", "Genes/targets"),
+          div(class = "ui form",
+  textAreaInput(inputId = "gene_list", label = "", value = "", placeholder = "MTOR\nRPS6K1\nAKT\n...\netc.")
+            ),
+  textOutput("gene_total"),
+  textOutput("search_genes"),
+          div(class = "ui button red", "Submit")
+            ),
+        div(class = "column",
+          a(class = "ui red ribbon label", "Probes"),
+          div(class = "ui form",
+            div(class = "ui raised segment",
+  checkboxGroupInput("probes", "", choiceNames = c("Best Class", "Second Class", "Non-specific", "Unknown Selectivity"), choiceValues = c("best", "second", "non", "un"))
+            )),
+            a(class = "ui red ribbon label", "Clinical Contribution"),
+          div(class = "ui form",
+            div(class = "ui raised segment",
+  checkboxGroupInput("clinical", "", choiceNames = c("Approved", "Phase I", "Phase II", "Phase III"), choiceValues = c("approved", "one", "two", "three"))
+            )
+          )
         ),
-    textOutput("gene_total"),
-    textOutput("search_genes"),
-    div(class = "ui button red", "Submit")
+        div(class = "column",
+          a(class = "ui red ribbon label", "Legacy compounds"),
+          div(class = "ui form",
+              div(class = "ui raised segment",
+  checkboxGroupInput("legacy", "", choiceNames = c("Gray best inhibitor list", "chemicalprobes.org 4.0 star rating"), choiceValues = c("gray", "chem_probe"))
+            )
+          )
+        )
+      ),
+      div(class = "ui one column centered grid",
+          div(class = "column",
+              dataTableOutput("output_table")
+          )
+      )
+    )
   )
 )
